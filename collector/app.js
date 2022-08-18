13d72503f07f03e6139756660f7b5610e83795e3
Matthew Elphick <matt.a.elphy@gmail.com>
2022-08-18 23:11:53 +0100

    feat: Skeleton of collector for another time

--- collector/app.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const app = express();
-const port = process.env.PORT || 3001;
-
-app.get("/", (_req, res) => res.json({ hello: "world" }));
-
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
