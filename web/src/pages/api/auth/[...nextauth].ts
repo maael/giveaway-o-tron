018f471a3408d28a31a9ee166d92e338fde29ee2
Matthew Elphick <matt.a.elphy@gmail.com>
2022-08-18 08:16:30 +0100

    feat: Make the web flow to get tokens work

--- web/src/pages/api/auth/[...nextauth].ts
+++ web/src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,29 @@ export default NextAuth({
     TwitchProvider({
       clientId: process.env.TWITCH_ID || '',
       clientSecret: process.env.TWITCH_SECRET || '',
+      profile(profile) {
+        return {
+          id: profile.sub,
+          name: profile.preferred_username,
+          email: profile.email,
+          image: profile.picture,
+        }
+      },
     }),
     // ...add more providers here
   ],
+  callbacks: {
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account?.access_token
+        token.refreshToken = account?.refresh_token
+      }
+      return token
+    },
+    async session({ session, token, user: _user }) {
+      session.accessToken = token.accessToken
+      session.refreshToken = token.refreshToken
+      return session
+    },
+  },
 })
--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,3 +1,29 @@
+import Image from 'next/image'
+import { useSession } from 'next-auth/react'
+
 export default function Index() {
-  return <div className="bg-purple-300 text-purple-800 flex justify-center items-center">🎉Next.js template🎉</div>
+  const session = useSession({ required: true })
+  return (
+    <div className="bg-gray-800 text-white flex flex-col gap-2 justify-center items-center">
+      {session?.data?.user?.image ? <Image src={session?.data?.user?.image} height={150} width={150} /> : null}
+      <div>Hey {session?.data?.user?.name}!</div>
+      <div className="flex flex-row">
+        <div className="px-2 py-1 bg-gray-600 rounded-l-md">Access Token</div>
+        <input
+          className="px-2 py-1 bg-gray-300 rounded-r-md text-gray-800 overflow-ellipsis"
+          readOnly
+          value={(session.data as any)?.accessToken}
+        />
+      </div>
+      <div className="flex flex-row">
+        <div className="px-2 py-1 bg-gray-600 rounded-l-md">Refresh Token</div>
+        <input
+          className="px-2 py-1 bg-gray-300 rounded-r-md text-gray-800 overflow-ellipsis"
+          readOnly
+          value={(session.data as any)?.refreshToken}
+        />
+      </div>
+      <div className="mt-5 text-sm">To finish setup, enter these tokens on the app.</div>
+    </div>
+  )
 }
