346678d758cdf7d273d973998dad247af42634bd
Matt Elphick <matthew.elphick@shopify.com>
2022-08-20 17:16:20 +0100

    feat: Add relay service

---
+++ relay/src/app.ts
@@ -0,0 +1,32 @@
+import express from "express";
+import http from "http";
+import { Server } from "socket.io";
+
+const app = express();
+const server = http.createServer(app);
+const port = process.env.PORT || 3001;
+
+const io = new Server(server);
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+  socket.on("event", (msg) => {
+    console.log("broadcast");
+    socket.broadcast.emit("event", msg);
+  });
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
+});
+
+app.get("/", (_req, res) => {
+  res.send(
+    `<h1>${io.engine.clientsCount} client${
+      io.engine.clientsCount === 1 ? "" : "s"
+    }</h1>`
+  );
+});
+
+server.listen(port, () => {
+  console.log(`http://localhost:${port}`);
+});
