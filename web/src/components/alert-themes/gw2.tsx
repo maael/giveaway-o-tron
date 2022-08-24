a4db527d001f36ce0efaea099925df0d0f68b4af
Matthew Elphick <matt.a.elphy@gmail.com>
2022-08-24 09:23:45 +0100

    feat: Make web aware of alert settings

--- web/src/components/alert-themes/gw2.tsx
+++ web/src/components/alert-themes/gw2.tsx
@@ -17,6 +17,7 @@ const canvasStyles = {
 export default function GW2Alerts() {
   const router = useRouter()
   const channel = router.query.channel
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
   const refAnimationInstance = React.useRef<any>(null)
 
   const getInstance = React.useCallback((instance) => {
@@ -34,7 +35,7 @@ export default function GW2Alerts() {
   }, [])
 
   const fire = React.useCallback(() => {
-    makeShot(0.25, {
+    makeShot(0.2, {
       spread: 80,
       decay: 0.9,
       scalar: 0.8,
@@ -43,29 +44,35 @@ export default function GW2Alerts() {
     })
   }, [makeShot])
   const handleEvent = React.useCallback(
-    (e: { winner: string; channelId: number }) => {
+    (e: { winner: string; channelId: number; alertDuration: number; alertTheme: string; type?: string }) => {
+      console.info('[event]', e)
       if (!channel || !e.channelId || (e.channelId || '').toString() !== (channel?.toString() || '')) return
-      toast.remove()
-      toast.custom((t) => {
-        if (t.visible) fire()
-        return (
-          <div className="flex flex-col justify-center items-center bg-transparent animate-wiggle relative">
-            <img src="/images/chest-notification.png" />
-            <div
-              className="text-white text-4xl uppercasetext-bold left-0 right-0 text-center absolute"
-              style={{ top: 230 }}
-            >
-              Giveaway chest!
-            </div>
-            <div
-              className="text-white text-3xl uppercase px-4 py-2 text-bold text-center absolute break-all"
-              style={{ top: 300, left: 50, right: 50 }}
-            >
-              {e.winner} won!
-            </div>
-          </div>
+      if (!e.type || e.type === 'winner') {
+        toast.remove()
+        toast.custom(
+          (t) => {
+            if (t.visible) fire()
+            return (
+              <div className="flex flex-col justify-center items-center bg-transparent animate-wiggle relative">
+                <img src="/images/chest-notification.png" />
+                <div
+                  className="text-white text-4xl uppercasetext-bold left-0 right-0 text-center absolute"
+                  style={{ top: 230 }}
+                >
+                  Giveaway chest!
+                </div>
+                <div
+                  className="text-white text-3xl uppercase px-4 py-2 text-bold text-center absolute break-all"
+                  style={{ top: 300, left: 50, right: 50 }}
+                >
+                  {e.winner} won!
+                </div>
+              </div>
+            )
+          },
+          { duration: e.alertDuration || 4000 }
         )
-      })
+      }
     },
     [fire, channel]
   )
