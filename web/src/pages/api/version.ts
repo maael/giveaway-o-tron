c97436c449be3bbcb9f828b9730804cf6cf0f1b7
Matthew Elphick <matt.a.elphy@gmail.com>
2022-08-26 22:26:55 +0100

    feat: Pass through neu resource url in versions endpoint

--- web/src/pages/api/version.ts
+++ web/src/pages/api/version.ts
@@ -4,11 +4,14 @@ import cors from '~/functions/cors'
 
 const handler: NextApiHandler = async (_req, res) => {
   const data = await fetch('https://api.github.com/repos/maael/giveaway-o-tron/releases/latest').then((r) => r.json())
+  const resourceUrl = data.assets.find((a) => a.name.endsWith('.neu'))?.browser_download_url
+  const resourceLocationRequest = await fetch(resourceUrl)
   res.json({
     version: (data.tag_name || '').replace('v', ''),
-    resource_url: data.assets.find((a) => a.name.endsWith('.neu'))?.browser_download_url,
+    resource_url: resourceLocationRequest.url || resourceUrl,
     applicationId: 'js.giveaway.otron',
     data: {
+      niceResourceUrl: resourceUrl,
       url: data.html_url,
       publishedAt: data.published_at,
       body: data.body,
