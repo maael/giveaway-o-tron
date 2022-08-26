2b9289c33ea3586c4448b4e1aa7a7bf663444a1e
Matthew Elphick <matt.a.elphy@gmail.com>
2022-08-26 23:03:56 +0100

    chore: More work on hosting version file

--- app/src/utils/updates.tsx
+++ app/src/utils/updates.tsx
@@ -6,16 +6,16 @@ export const APP_VERSION: string | undefined = (globalThis as any).NL_APP_VERSIO
 export async function getLatestDifferentRelease() {
   try {
     const currentVersion = APP_VERSION
-    const data = await fetch(`https://api.github.com/repos/maael/giveaway-o-tron/releases/latest`).then((res) =>
-      res.json()
-    )
+    const data = await fetch(`https://giveaway-o-tron.vercel.app/api/version`).then((res) => res.json())
     const updateInfo = {
-      name: data.name,
-      url: data.html_url,
-      body: data.body,
+      name: `v${data.version}`,
+      resourceUrl: data.resource_url,
+      url: data.data.url,
+      body: data.data.body,
     }
     console.info('[update][latest]', updateInfo, currentVersion)
     if (updateInfo.name !== `v${currentVersion}`) {
+      console.info('[update][new]', { current: `v${currentVersion}`, new: updateInfo.name })
       return updateInfo
     }
     return null
@@ -28,15 +28,40 @@ export async function getLatestDifferentRelease() {
 export function useUpdateCheck() {
   React.useEffect(() => {
     ;(async () => {
-      const latestRelease = await getLatestDifferentRelease()
-      if (latestRelease)
-        toast((t) => {
-          return (
-            <button onClick={() => Neutralino.os.open(latestRelease?.url)} className="underline text-purple-600">
-              Update {latestRelease?.name} available, go to download →
-            </button>
-          )
-        })
+      try {
+        const newVersion = await getLatestDifferentRelease()
+        if (newVersion) {
+          toast((t) => {
+            return (
+              <div className="flex flex-row gap-4 justify-center items-center">
+                <button onClick={() => Neutralino.os.open(newVersion.url)} className="underline text-purple-600">
+                  {newVersion.name} available →
+                </button>
+                or
+                <button
+                  className="bg-purple-600 px-2 py-1 rounded-md text-white hover:scale-105"
+                  onClick={async () => {
+                    const original = await fetch(newVersion.resourceUrl)
+                    console.info('status', original.status)
+                    const resourceUrl = original.headers['location']
+                    if (!resourceUrl) {
+                      console.warn('Failed to find resourceUrl')
+                      return
+                    }
+                    const data = await fetch(resourceUrl).then((data) => data.arrayBuffer())
+                    await Neutralino.filesystem.writeBinaryFile('/test.res', data)
+                    await Neutralino.app.restartProcess()
+                  }}
+                >
+                  Update now
+                </button>
+              </div>
+            )
+          })
+        }
+      } catch (e) {
+        console.warn('[update][error]', e)
+      }
     })()
   }, [])
 }
