23c8dc7cfb334b55b9ffca28d370001052d25662
Matthew Elphick <matt.a.elphy@gmail.com>
2022-08-26 21:59:08 +0100

    feat: Add version endpoint to web

---
+++ web/src/pages/api/version.ts
@@ -0,0 +1,18 @@
+import { NextApiHandler } from 'next'
+import fetch from 'isomorphic-fetch'
+
+const handler: NextApiHandler = async (_req, res) => {
+  const data = await fetch('https://api.github.com/repos/maael/giveaway-o-tron/releases/latest').then((r) => r.json())
+  res.json({
+    version: (data.tag_name || '').replace('v', ''),
+    resource_url: data.assets.find((a) => a.name.endsWith('.neu'))?.browser_download_url,
+    applicationId: 'js.giveaway.otron',
+    data: {
+      url: data.html_url,
+      publishedAt: data.published_at,
+      body: data.body,
+    },
+  })
+}
+
+export default handler
