0458ceda875f42c1bbc73780e47f7dbfa46554e3
Matthew Elphick <matt.a.elphy@gmail.com>
2023-09-30 18:16:29 +0100

    chore: Update script to update exe info

--- app/scripts/update-exe.js
+++ app/scripts/update-exe.js
@@ -1,12 +1,34 @@
 const rcedit = require('rcedit')
+const fs = require('fs').promises
 const path = require('path')
 const icoPath = path.join(__dirname, '..', 'resources', 'icons', 'trayIcon.ico')
 const exePath = path.join(__dirname, '..', 'dist', 'giveaway-o-tron', 'giveaway-o-tron.exe')
 const getVersion = require('./util/version')
 
+const wait = (ms) => new Promise((r) => setTimeout(r, ms))
+
+async function confirmFile(name, path) {
+  return fs
+    .stat(path)
+    .then(() => {
+      console.info(`[exe] ${name} exists`)
+      return true
+    })
+    .catch((e) => {
+      console.error(`[exe] Failed to find ${name}`, e)
+      return false
+    })
+}
+
 ;(async () => {
   console.info('[exe:start]')
+  console.info('[exe] CWD', process.cwd())
+  console.info('[exe] dirname', __dirname)
   const version = getVersion()
+  await wait(1_000)
+  const iconExists = await confirmFile('icon', icoPath)
+  const exeExists = await confirmFile('exe', exePath)
+  if (!iconExists || !exeExists) throw new Error('Missing required file')
   await rcedit(exePath, {
     icon: icoPath,
     'file-version': version,
